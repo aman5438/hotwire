91fbfb6b2607c1439e6b0b929c82105e826897a6
aman agrawal <amanagrawal5438@gmail.com>
2023-11-24 18:14:23 +0530

    project setup

---
+++ config/tailwind.config.js
@@ -0,0 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('tailwindcss'),
+    ],
+}
